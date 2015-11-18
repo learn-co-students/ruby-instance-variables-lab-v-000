c8b736e5cfad025a69d8eb786ec38fafe94600f3
Justin Thomas <jthomas0209@gmail.com>
2015-11-18 10:13:59 -0500

    Created Dog class with name get and set

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  
+  def initialize(input = "Dog")
+    @this_dogs_name = input
+  end
+  
+  def name=(input)
+    @this_dogs_name = input
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+end
+

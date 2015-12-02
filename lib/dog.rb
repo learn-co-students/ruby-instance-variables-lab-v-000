548d77b69fc845e05d71e9dbeb3dc730d4881956
authorbeard <msnolan@gmail.com>
2015-12-02 15:36:35 -0500

    all tests passing

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  def name=(name)
+    @this_dogs_name = name
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+ 
+
+
+  
+
+end

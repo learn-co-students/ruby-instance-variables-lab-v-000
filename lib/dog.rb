a7c93208f3c724d83280b2cd926ee521760f0445
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-10 17:37:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,3 +10,7 @@ end
 
 lassie = Dog.new
 lassie.name = "Lassie"
+
+#Why did it work? Inside the #name= method, we set the value of @this_dogs_name equal to whatever string is passed in as an argument. Then, we are able to call on that same instance variable in a totally separate method, the #name method.
+#CONCLUSION
+#As we dive deeper into object oriented Ruby, we'll be using instance variables frequently to pass information around the instance methods of a class. Think of instance variables as the containers for instance-specific information. The ability of instance variables to store information and be accessible within different instance methods is one of the things that makes it possible for us to create similar, but unique objects in object orientated Ruby.

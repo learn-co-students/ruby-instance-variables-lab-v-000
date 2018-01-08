a3b016b8e96d294d6f6c1ef9d448de1e5e283c84
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-08 14:55:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def name= (dog_name)
+  def name= (dog_name) #the setter or writer method
     @this_dogs_name = dog_name
   end
 
-  def name
+  def name #the getter or reader method
     @this_dogs_name
   end
 end

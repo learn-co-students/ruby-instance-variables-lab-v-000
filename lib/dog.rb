0f4b8f65821884a69b4ff1206105db558262647d
Laura Darlak <laura@polyporedesign.com>
2018-08-23 00:26:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+
+class Dog
+
+  # writer method
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  # reader method
+  def name
+    @this_dogs_name
+  end
+
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name

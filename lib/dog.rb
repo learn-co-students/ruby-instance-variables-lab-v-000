c47f1078236ed15942f4a32ee6b48729e1a60f58
Tim Knoll <timothy.knoll@gmail.com>
2018-09-18 21:10:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  def name=(dog_name) # setting
+  def name=(dog_name) # setting - > sets instance variable
     @this_dogs_name = dog_name
   end
 
-  def name # getting
+  def name # getting - > calls on instance variable
     @this_dogs_name
   end
 

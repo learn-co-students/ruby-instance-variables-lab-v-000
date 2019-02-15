491acd6d01b5482c2bb19523307c407c77aa4776
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-02-15 02:38:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
 require 'pry'
 class Dog
+  attr_accessor :name
+  #def name=(this_dogs_name)
+    #@this_dogs_name = this_dogs_name
+  #end
   
-  
-  def name=(this_dogs_name)
-    @this_dogs_name = this_dogs_name
-  end
-  
-  def name
-    @this_dogs_name
-  end
+  #def name
+    #@this_dogs_name
+  #end
   
   #binding.pry
 end 
